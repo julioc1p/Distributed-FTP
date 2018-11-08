09ec484f014847b2c14459e9c1ccaf2d8a2c2ecb
Julio Cesar Sanchez Garcia <julio.c.hxh@gmail.com>
2018-11-08 15:07:58 -0500

    Agregando 'file system os' y 'utils' a la raiz (import problems)

---
+++ file_system_os.py
@@ -0,0 +1,23 @@
+from file_system import FileSystem
+from utils import fileProperty
+import os
+
+class FileSystemOS(FileSystem):
+
+    # def __init__(self):
+    #     FileSystem.__init__(self)
+
+
+    def list(self, pathname):
+        if self.isdir(pathname):
+            return os.listdir(pathname)
+        return []
+
+    def exists(self, pathname):
+        return os.path.exists(pathname)
+
+    def fileProperty(self, filepath):
+        return fileProperty(filepath)
+
+    def isdir(self, pathname):
+        return os.path.isdir(pathname)
