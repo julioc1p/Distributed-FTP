e59da384b8982abc16564e0058567811f23220dc
Julio Cesar Sanchez Garcia <julio.c.hxh@gmail.com>
2018-11-07 20:01:51 -0500

    Arreglando los comentarios del file system

--- file_system.py
+++ file_system.py
@@ -1,43 +1,46 @@
 class FileSystem():
+    """
+    Clase que define la estructura de un sistema de ficheros
+    """
 
-    """
-    Lista los archivos que hay en el path
-    """
     def list(self, path):
+        """
+        Lista los archivos que hay en el path
+        """
         pass
 
-    """
-    Envia el archivo indicado en el path
-    """
     def send(self, path):
+        """
+        Envia el archivo indicado en el path
+        """
         pass
 
-    """
-    Comienza a recivir un archivo por el socket
-    """
     def recv(self, socket):
+        """
+        Comienza a recivir un archivo por el socket
+        """
         pass
 
-    """
-    Elimina el archivo indicado por el path
-    """
     def delete(self, path):
+        """
+        Elimina el archivo indicado por el path
+        """
         pass
 
-    """
-    Crea un nuevo archivo en el path indicado
-    """
     def create(self, path):
+        """
+        Crea un nuevo archivo en el path indicado
+        """
         pass
 
-    """
-    Devuelve la cantidad de espacio disponible en el sistema
-    """
     def alloc(self):
+        """
+        Devuelve la cantidad de espacio disponible en el sistema
+        """
         pass
 
-    """
-    Renombra el archivo designado por el path con el nuevo nombre
-    """
     def rename(self, path, new_name):
+        """
+        Renombra el archivo designado por el path con el nuevo nombre
+        """
         pass
